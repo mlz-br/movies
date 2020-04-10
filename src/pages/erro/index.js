54d7d6375f9d4bc1aaa687d7128b6e801b738a6f
mlz-br <m.z@outlook.com.br>
2020-04-10 11:41:25 -0300

    Fontes movie - inicial

---
+++ src/pages/erro/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Erro extends Component {
+    render(){
+        return(
+            <div> Página não encontrada!</div>
+        )
+    }
+
+}
+
+export default Erro;
---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Header from './components/Header';
+import Filme from './pages/Filme';
+import Erro from './pages/erro';
+
+const Routes = () => {
+    return (
+        <BrowserRouter>
+           <Header/>
+           <Switch>
+               <Route exact path = "/" component = { Home } />
+               <Route exact path="/filme/:id" component = { Filme } />
+               <Route path = "*" component = {Erro} />
+           </Switch>
+        </BrowserRouter>
+    )
+
+}
+export default Routes;
